6a3fc176994470e2c55350288b7b4515dac3da86
Daveen Knue <daveen.c.knue@gmail.com>
2023-10-29 13:39:12 -0400

    FizzBuzz Corrected

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
-x = int(input("Enter number from 0-100:"))
-if (x % 3 == 0) and (x % 5 == 0):
-    print("FizzBuzz")
-elif (x % 3 == 0):
-    print("Fizz")
-elif (x % 5 == 0):
-    print("Buzz")
+for x in range(1,101):
+    if (x % 3 == 0) and (x % 5 == 0):
+        print("FizzBuzz")
+    elif (x % 3 == 0):
+        print("Fizz")
+    elif (x % 5 == 0):
+        print("Buzz")
+    else:
+        print(x)
